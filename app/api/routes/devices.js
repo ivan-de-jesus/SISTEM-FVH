60dc85a6517f6af12ae1fc3cf9703a8670a37ef8
Luis Antonio TM <antonio_code@protonmail.ch>
2021-09-30 21:28:48 -0500

    Cambios menores

--- app/api/routes/devices.js
+++ app/api/routes/devices.js
@@ -3,12 +3,60 @@ const router = express.Router();
 
 //localhost:3001/api1/device 
 
-router.get("/device", (req, res) =>{
-   
+import Device from '../models/device.js';
+
+router.get("/device", async, (req, res) =>{
+
+    try {
+        const userId = req.userData._id;
+    
+        const devices = await Device.find({ userId: userId });
+
+        const toSend = {
+            status: "success",
+            data: devices
+        };
+
+        res.json(toSend);
+
+    } catch (error) {
+        const toSend = {
+          status: "error",
+          error: error
+        }
+    
+        return res.status(500).json(toSend);
+    }
+
 });
 
-router.get("/device", (req, res) =>{
-   
+router.post("/device", async, (req, res) =>{
+    try {
+        const userId = req.userData._id;
+        var newDevice = req.body.newDevice;
+    
+        newDevice.userId = userId;
+        newDevice.createdTime = Date.now();
+    
+        const device = await Device.create(newDevice);
+    
+        const toSend = {
+          status: "success"
+        }
+    
+        return res.json(toSend);
+    
+      } catch (error) {
+        console.log("ERROR CREATING NEW DEVICE");
+        console.log(error);
+    
+        const toSend = {
+          status: "error",
+          error: error
+        }
+    
+        return res.status(500).json(toSend);
+    }
 });
 
 router.delete("/device", (req, res) =>{
